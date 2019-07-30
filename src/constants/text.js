9869780e265352ee09b0f73c2d3ba44d3528d68f
okyry <okyry@softserveinc.com>
2019-07-30 11:45:20 +0300

    add text constants for i18n

---
+++ src/constants/text.js
@@ -0,0 +1,19 @@
+export const EN = {
+  ADMIN_PANEL_TEXT: 'Admin Panel',
+  FOOTER_TEXT: 'Footer',
+
+  //----------IMAGES---------
+  IMAGES_TITLE: 'Images',
+  ADD_PICTURE: 'Add picture',
+  UPLOAD_TEXT: 'upload',
+
+  //----------USERS---------
+  USERS_TITLE: 'Users',
+  ADD_USER: 'Add user',
+  LOGIN: 'login',
+  PASSWORD: 'password',
+  AVATAR: 'avatar',
+  GENDER: 'gender',
+  MALE: 'male',
+  FEMALE: 'female',
+};
