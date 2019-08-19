6ba5cfe938a404737982223c1cf3f46cf5f6f628
okyry <okyry@softserveinc.com>
2019-08-19 12:21:57 +0300

    add css to form layout (flex => row)

--- src/shared/components/form/formBlock/layouts/layout1.jsx
+++ src/shared/components/form/formBlock/layouts/layout1.jsx
@@ -9,17 +9,17 @@ const Layout1Component = (props) => {
 
   console.log('data', props.data);
   return (
-    <Box display={'flex'} flexDirection={'column'} width={1} flex={1} >
+    <Box display={'flex'} flexDirection={'row'} justifyContent={'space-between'} width={1} flex={1} >
       {
         get(props,'formConfig.position1')&&
-        <Box display={'flex'} flexDirection={'column'} width={1} flex={1} >
+        <Box display={'flex'} flexDirection={'column'} width={1} flex={3} >
           { get(props,'formConfig.position1.formFields',[])
             .map((formField)=> getFormToggleField( {formField, data: props.data} )) }
         </Box>
       }
       {
         get(props,'formConfig.position2')&&
-        <Box display={'flex'} flexDirection={'column'} width={1} flex={1} >
+        <Box display={'flex'} flexDirection={'column'} width={1} flex={3} >
           { get(props,'formConfig.position2.formFields',[])
             .map((formField)=> getFormToggleField( {formField, data:props.data} )) }
         </Box>
